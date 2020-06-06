4193e3041c1a429f17fb1f2a3e119cec92777db1
jordancheng604 <jordancheng604@gmail.com>
2020-06-05 20:15:49 -0700

    Progress being made. Biography page comment section is moving ahead; displays on page, but comment layout needs correcting. And have to figure out the giant grey dot/pictire icon-logo.

---
+++ BandSite-Sprint-2/javascript/biography.js
@@ -0,0 +1,73 @@
+console.log('check to work work?');
+
+const arr1=[
+    {
+        Name: "Michael Lyons",
+        Date: "12/18/2018",
+        Comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+    },
+
+    {
+        Name: "Gary Wong",
+        Date: "12/12/2018",
+        Comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!",
+    },
+
+    {
+        Name: "Theodore Duncan",
+        Date: "11/15/2018",
+        Comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+    },
+]
+
+
+let parent = document.getElementById("commentForm");
+
+for(let i=0;i<arr1.length;i++){
+    let showCard = document.createElement('li');
+    showCard.classList.add("show__date2");
+    parent.appendChild(showCard);
+    document.querySelectorAll(".show__date2")[i].innerHTML = arr1[i].Name
+
+    let Card2 = document.createElement('div');
+    Card2.classList.add("show__venue1");
+    showCard.appendChild(Card2);
+    document.querySelectorAll('.show__venue1')[i].innerHTML = arr1[i].Name
+
+
+    let showCard2 = document.createElement('div');
+    showCard2.classList.add("show__venue2");
+    showCard.appendChild(showCard2);
+    document.querySelectorAll('.show__venue2')[i].innerHTML = arr1[i].Date
+
+
+    let showCard3 = document.createElement('div');
+    showCard3.classList.add("show__venue3");
+    showCard.appendChild(showCard3);
+    document.querySelectorAll('.show__venue3')[i].innerHTML = arr1[i].Comment
+    
+}
+
+
+
+let displayComment = document.getElementById('commentForm');
+displayComment.addEventListener("submit", function (event){
+let commentButton = document.getElementById('commentButton');
+console.log('Comment button', event);
+event.preventDefault();
+console.log('Comment button clicked!')
+let name1 = document.createElement('div');
+let input = event.target.nameInput.value;
+let name2 = document.createElement('div');
+let input2 = event.target.textArea.value;
+name1.innerText=input;
+name2.innerText=input2;
+let list = document.getElementById('commentList');
+list.appendChild(name1);
+list.appendChild(name2);
+document,this.querySelector('input', 'input2').value = "";
+// document,this.querySelector('input2').value = "";
+
+
+}
+)
